bef034c80a98f2da4c6b40b3a607f7de8c553c3f
Destiinc <Dechoi1@s.sfusd.edu>
2026-02-03 14:15:58 -0800

    Lesson 5 Complete

--- lessons/chest/chest.gd
+++ lessons/chest/chest.gd
@@ -1,13 +1,31 @@
 extends Area2D
 
 @onready var canvas_group: CanvasGroup = $CanvasGroup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 
 func _ready() -> void:
 	canvas_group.material.set_shader_parameter("line_thickness", 7.0)
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 	
-	
+func open() -> void:
+	if animation_player.assigned_animation == "open":
+		return
+	input_pickable = false
+	animation_player.play("open")
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_index: int):
+	var event_is_mouse_click: bool = (
+		event is InputEventMouseButton and
+		event.button_index == MOUSE_BUTTON_LEFT and
+		event.is_pressed()
+	)
+
+	if event_is_mouse_click:
+		open()
+
 func _on_mouse_entered() -> void:
 	var tween := create_tween()
 	tween.tween_method(set_outline_thickness, 7.0, 12.0, 0.08)
